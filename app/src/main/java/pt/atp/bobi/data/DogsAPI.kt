af7f86d46ccad723ae70c8a8c91b512fa3aab506
Rafael Oliveira <rafaelcardosooliveira0@gmail.com>
2020-12-16 14:35:28 +0000

    converted activities in fragments

--- app/src/main/java/pt/atp/bobi/data/DogsAPI.kt
+++ app/src/main/java/pt/atp/bobi/data/DogsAPI.kt
@@ -3,7 +3,6 @@ package pt.atp.bobi.data
 import pt.atp.bobi.data.model.Breed
 import pt.atp.bobi.data.model.Search
 import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
